f02b3611de55ba3379f836315f2f05e2f5dc1244
Thabo Maduna <57966143+MadunaThabo@users.noreply.github.com>
2021-11-08 22:15:17 +0200

    Mark2Creater.h and cpp

--- Dragon Spacecraft/code/Mark2Creator.cpp
+++ Dragon Spacecraft/code/Mark2Creator.cpp
@@ -1,12 +1,9 @@
 #include "Mark2Creator.h"
 
 Mark2Creator::Mark2Creator() {
-	// TODO - implement Mark2Creator::Mark2Creator
-	throw "Not yet implemented";
+	
 }
 
 Parachute* Mark2Creator::createParachute() {
-	// TODO - implement Mark2Creator::createParachute
-	throw "Not yet implemented";
-	
+	return new Mark2Parachute();
 }
--- Dragon Spacecraft/code/Mark2Creator.h
+++ Dragon Spacecraft/code/Mark2Creator.h
@@ -1,13 +1,12 @@
 #ifndef MARK2CREATOR_H
 #define MARK2CREATOR_H
+#include "Mark2Parachute.h"
 
 class Mark2Creator : ParachuteCreator {
 
 
 public:
 	Mark2Creator();
-
 	Parachute* createParachute();
-	// final test
 };
 #endif
