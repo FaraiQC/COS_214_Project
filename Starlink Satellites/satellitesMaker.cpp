ba5b659d69b561b7f4a67ab3a2101d364f93240b
Kuda Christine <62636817+Kuda214@users.noreply.github.com>
2021-11-08 15:31:25 +0200

    Update satellitesMaker.cpp

--- Starlink Satellites/satellitesMaker.cpp
+++ Starlink Satellites/satellitesMaker.cpp
@@ -5,9 +5,26 @@ satellitesMaker::satellitesMaker() {
 }
 
 void satellitesMaker::CollectNextCluster() {
+	//var
+	Satellite* p = new Satellite() ;
+	
 	if(isEmpty() == true)
 	{
 		cout<<"There are no StarLink Satellites to collect."<<endl;
+	}
+	
+	//make 1 Satellite
+	this->first = p;
+	first->prevS = null;
+	//prototype 59 Satellites using prototype and iterator 
+	Satellite* cur = first;
+	Satellite* prev = null;
+	
+	
+	
+	for (int n=0;n<60;n++)
+	{
+	   first->nextS = p->clone();
 	}
 	
 	cout<<"60 Satellites have been collected by the Falcon 9 Rocket."<<endl;
