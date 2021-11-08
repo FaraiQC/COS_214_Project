3a379e1e1b1eae84989c6aa64cbd0aa641b21e25
Farai <84129983+FaraiQC@users.noreply.github.com>
2021-11-08 16:39:30 +0200

    Update Satellite.cpp

--- Starlink Satellites/Satellite.cpp
+++ Starlink Satellites/Satellite.cpp
@@ -1,35 +1,36 @@
 #include "Satellite.h"
 
-Satellite::~Satellite() {
-	// TODO - implement Satellite::~Satellite
-	throw "Not yet implemented";
+Satellite::~Satellite() 
+{
+
 }
 
-Satellite::Satellite() {
+Satellite::Satellite() 
+{
 	this-> distance = 0.0;
 }
 
-Satellite* Satellite::nextSatellite() {
-	// TODO - implement Satellite::nextSatellite
-	throw "Not yet implemented";
+Satellite* Satellite::nextSatellite() 
+{
+
 }
 
-Satellite* Satellite::prevSatellite() {
-	// TODO - implement Satellite::prevSatellite
-	throw "Not yet implemented";
+Satellite* Satellite::prevSatellite() 
+{
+
 }
 
-void Satellite::attach(Antenna antenna, Antenna antenna) {
-	// TODO - implement Satellite::attach
-	throw "Not yet implemented";
+void Satellite::attach(Antenna antenna, Antenna antenna) 
+{
+
 }
 
-void Satellite::detach(Antenna antenna, Antenna antenna) {
-	// TODO - implement Satellite::detach
-	throw "Not yet implemented";
+void Satellite::detach(Antenna antenna, Antenna antenna) 
+{
+
 }
 
-void Satellite::notifyAntenna() {
-	// TODO - implement Satellite::notifyAntenna
-	throw "Not yet implemented";
+void Satellite::notifyAntenna() 
+{
+
 }
