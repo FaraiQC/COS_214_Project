b222a81c082bfb16f191aba283e8d02224c887d1
Farai <84129983+FaraiQC@users.noreply.github.com>
2021-11-08 16:37:05 +0200

    Update Antenna.cpp

--- Starlink Satellites/Antenna.cpp
+++ Starlink Satellites/Antenna.cpp
@@ -1,11 +1,11 @@
 #include "Antenna.h"
 
-Antenna::Antenna() {
-	// TODO - implement Antenna::Antenna
-	throw "Not yet implemented";
+Antenna::Antenna() 
+{
+
 }
 
-Antenna::~Antenna() {
-	// TODO - implement Antenna::~Antenna
-	throw "Not yet implemented";
+Antenna::~Antenna() 
+{
+	
 }
