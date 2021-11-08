73a7c65c3eb9db5cfb7de703b468406b04ec42bc
Kuda Christine <62636817+Kuda214@users.noreply.github.com>
2021-11-08 21:11:25 +0200

    main

---
+++ Starlink Satellites/main.cpp
@@ -0,0 +1,21 @@
+#include "Antenna.h"
+#include "ConcreteAntenna.h"
+#include "ConcreteMediator.h"
+#include "ConcreteSatellite.h"
+#include "Satellite.h"
+#include "SatelliteMediator.h"
+#include "SatellitesMaker.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // SatellitesMaker* sat = new SatellitesMaker();
+
+    cout<<"Satellites waiting to be collected.\n";
+    
+
+    return 0;
+}
