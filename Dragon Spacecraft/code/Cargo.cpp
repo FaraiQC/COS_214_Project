178ffa5bfee1eeef2a03ee1c0c0c3a3261f031df
Thabo Maduna <57966143+MadunaThabo@users.noreply.github.com>
2021-11-09 19:44:54 +0200

    Add files via upload

---
+++ Dragon Spacecraft/code/Cargo.cpp
@@ -0,0 +1,36 @@
+#include "Cargo.h"
+
+Cargo::Cargo() {
+	// TODO - implement Cargo::Cargo
+	throw "Not yet implemented";
+}
+
+string Cargo::getDescription() {
+	return this->description;
+}
+
+string Cargo::getName() {
+	// TODO - implement Cargo::getName
+	throw "Not yet implemented";
+}
+
+void Cargo::setDescription(string Name) {
+	this->description = Name;
+}
+
+void Cargo::setName(string name) {
+	// TODO - implement Cargo::setName
+	throw "Not yet implemented";
+}
+
+
+
+void Cargo::setWeight(double a){
+	this->weight = a;
+}
+
+double Cargo::getWeight(){
+
+}
+
+
