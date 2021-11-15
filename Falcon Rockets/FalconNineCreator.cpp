15a95164643a5d5283f8bbffdfca1ef1c94d7c1b
Farai <84129983+FaraiQC@users.noreply.github.com>
2021-11-15 21:35:52 +0200

    Delete Falcon Rockets directory

--- Falcon Rockets/FalconNineCreator.cpp
+++
@@ -1,9 +0,0 @@
-#include "FalconNineCreator.h"
-
-FalconNineCreator::FalconNineCreator() = default;
-
-Rocket *FalconNineCreator::createRocket() {
-    return new FalconNine();
-}
-
-FalconNineCreator::~FalconNineCreator() = default;
