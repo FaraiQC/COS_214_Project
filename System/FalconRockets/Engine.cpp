a12176e63c6826797ca63bcc9aec8c4946b6d332
Musa-Mabasa <93927619+Musa-Mabasa@users.noreply.github.com>
2021-11-16 09:52:36 -0600

    Add files via upload

---
+++ System/FalconRockets/Engine.cpp
@@ -0,0 +1,43 @@
+#include "Engine.h"
+
+Engine::Engine(string EngineId, int maxThrust, string propellant) {
+    
+}
+
+string Engine::getId() {
+    
+}
+
+void Engine::setId(string id) {
+    
+}
+
+string Engine::getPropellant() {
+    
+}
+
+void Engine::setPropellant(string prop) {
+    
+}
+
+bool Engine::getState() {
+   
+}
+
+void Engine::turnOn() {
+
+    
+}
+
+void Engine::turnOff() {
+
+   
+}
+
+void Engine::setState(bool state) {
+    
+}
+
+Engine::~Engine() = default;
+
+
---
+++ System/FalconRockets/Engine.h
@@ -0,0 +1,37 @@
+#ifndef FALCON_ROCKETS_ENGINE_H
+#define FALCON_ROCKETS_ENGINE_H
+
+#include "iostream"
+
+using namespace std;
+
+class Engine {
+private:
+    string id;
+    bool on;
+    int thrustMax;
+    string propellant;
+public:
+    explicit Engine(string Id, int maxThrust, string propellant);
+
+    string getId();
+
+    void setId(string id);
+
+    string getPropellant();
+
+    bool getState();
+
+    void setState(bool);
+
+    void setPropellant(string propellant);
+
+    virtual void turnOn();
+
+    virtual void turnOff();
+
+    virtual ~Engine();
+};
+
+
+#endif
