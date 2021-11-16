3092a7dc4ebad0cc6af8b1b6687d11f6e20169b3
Farai <84129983+FaraiQC@users.noreply.github.com>
2021-11-16 15:05:39 +0200

    Update TestSatellites.cpp

--- System/LaunchSimulator/TestSatellites.cpp
+++ System/LaunchSimulator/TestSatellites.cpp
@@ -1,21 +1,21 @@
 #include "../../../../../../Documents/VPProjects/TestSatellites.h"
 
-TestSatellites::TestSatellites() {
-	// TODO - implement TestSatellites::TestSatellites
-	throw "Not yet implemented";
+TestSatellites::TestSatellites() 
+{
+	
 }
 
-TestSatellites::~TestSatellites() {
-	// TODO - implement TestSatellites::~TestSatellites
-	throw "Not yet implemented";
+TestSatellites::~TestSatellites() 
+{
+	
 }
 
-void TestSatellites::BeforeLaunch() {
-	// TODO - implement TestSatellites::BeforeLaunch
-	throw "Not yet implemented";
+void TestSatellites::BeforeLaunch() 
+{
+	
 }
 
-void TestSatellites::InDesiredOrbit() {
-	// TODO - implement TestSatellites::InDesiredOrbit
-	throw "Not yet implemented";
+void TestSatellites::InDesiredOrbit() 
+{
+	
 }
