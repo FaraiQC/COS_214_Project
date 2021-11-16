c55eed9f7354332229243868bdc3e2a823539f54
Farai <84129983+FaraiQC@users.noreply.github.com>
2021-11-16 15:02:58 +0200

    Update TestRockets.cpp

--- System/LaunchSimulator/TestRockets.cpp
+++ System/LaunchSimulator/TestRockets.cpp
@@ -1,36 +1,36 @@
 #include "../../../../../../Documents/VPProjects/TestRockets.h"
 
-TestRockets::TestRockets() {
-	// TODO - implement TestRockets::TestRockets
-	throw "Not yet implemented";
+TestRockets::TestRockets() 
+{
+	
 }
 
-TestRockets::~TestRockets() {
-	// TODO - implement TestRockets::~TestRockets
-	throw "Not yet implemented";
+TestRockets::~TestRockets() 
+{
+	
 }
 
-void TestRockets::Before_Launch() {
-	// TODO - implement TestRockets::Before Launch
-	throw "Not yet implemented";
+void TestRockets::Before_Launch() 
+{
+	
 }
 
-void TestRockets::ReleaseSatellites() {
-	// TODO - implement TestRockets::ReleaseSatellites
-	throw "Not yet implemented";
+void TestRockets::ReleaseSatellites() 
+{
+	
 }
 
-void TestRockets::ReleaseSpacecraft() {
-	// TODO - implement TestRockets::ReleaseSpacecraft
-	throw "Not yet implemented";
+void TestRockets::ReleaseSpacecraft() 
+{
+	
 }
 
-void TestRockets::AfterLaunch() {
-	// TODO - implement TestRockets::AfterLaunch
-	throw "Not yet implemented";
+void TestRockets::AfterLaunch() 
+{
+	
 }
 
-void TestRockets::Before_Launch() {
-	// TODO - implement TestRockets::Before Launch
-	throw "Not yet implemented";
+void TestRockets::Before_Launch() 
+{
+	
 }
