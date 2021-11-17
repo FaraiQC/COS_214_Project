96b880a607c6a272dc5f4fc7397ed8e651953814
Farai <84129983+FaraiQC@users.noreply.github.com>
2021-11-17 18:14:55 +0200

    Update ConcreteAntenna.cpp

--- System/StarlinkSatellites/ConcreteAntenna.cpp
+++ System/StarlinkSatellites/ConcreteAntenna.cpp
@@ -1,10 +1,12 @@
 #include "ConcreteAntenna.h"
 
+//@brief Sets radio Signal to equal the subjects signal
 void ConcreteAntenna::update() 
 {
   radioSignal = subject->getSignal();
 }
 
+//@brief Sets radio Signal to false
 ConcreteAntenna::ConcreteAntenna() 
 {
   cout << "All Antenna's Radio Signals are initially False" << endl;
