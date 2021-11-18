31ae76bd602a2cfbd8467738a641be877194665b
Farai <Farai.fantasy.822@gmail.com>
2021-11-19 00:21:59 +0200

    Update Parachute.cpp

--- System/DragonSpacecraft/code/Parachute.cpp
+++ System/DragonSpacecraft/code/Parachute.cpp
@@ -1,28 +1,51 @@
 #include "Parachute.h"
 
-Parachute::Parachute() {
+//@brief Constructor
+Parachute::Parachute() 
+{
 
 }
 
-Parachute::Parachute(string id){
+//@brief Sets the id of the Parachute
+//@param id is the id of the Parachute
+Parachute::Parachute(string id)
+{
 	setId(id);
 }
 
-void Parachute::setStoppingPower(int stoppingPower) {
+//@brief Sets the stopping power of the Parachuite
+//@param stoppingPower is the stopping Power of the parachute
+void Parachute::setStoppingPower(int stoppingPower) 
+{
 	this->stoppingPower = stoppingPower;
 }
 
-int Parachute::getStoppingPower() {
+//@brief gets the stopping power of the parachute
+//@return returns the stopping power of the parachute
+int Parachute::getStoppingPower() 
+{
 	return this->stoppingPower;
 }
 
-void Parachute::setId(string id){
+//@brief sets the id of the Parachute
+//@param id is the id of the parachute
+void Parachute::setId(string id)
+{
 	this->id = id;
 }
 
-string Parachute::getId(){
+//@brief gets the id of the parachute
+//@return returns the id of the parachute
+string Parachute::getId()
+{
 	return this->id;
 }
 
+//@brief Confirms that Mark2 Parachute was created
+//@param check is 0 when there was no parachute created, and 1 when there was a parachute created
 void Parachute::SetMark2isCreatedChecker(int chech){this->check=check;}
+
+//@brief Checks if there was a parachute created
+//@return returns 0 when there was no Parachute created
+//@return returns 1 when there was a Parachute created
 int Parachute::GetMark2isCreatedChecker(){return check;}
