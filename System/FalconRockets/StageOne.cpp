ed9207ba2dcfdad77a28a78bc32620c08b1f2baa
Musa-Mabasa <93927619+Musa-Mabasa@users.noreply.github.com>
2021-11-17 20:58:48 -0600

    Update StageOne.cpp

--- System/FalconRockets/StageOne.cpp
+++ System/FalconRockets/StageOne.cpp
@@ -1,12 +1,26 @@
 #include "StageOne.h"
 #include "thread"
-#include "chrono"
+#include <sstream>
+#include <unistd.h>
+#include <cstdlib>
+#include <ctime>
+#include <stdio.h>
 
 StageOne::StageOne(string id) : Stage(id) {
 }
 
-void StageOne::addEngine(Engine *engine) {
-    engines.push_back(engine);
+void StageOne::addEngine() {
+    for(int i=0;i<numEngines;i++)
+    {
+        string id="CoreEngine-";
+        string num;
+        stringstream ss;
+        ss<<i;
+        ss>>num;
+        id+=num;
+        Engine *engine = new MerlinCore(id);
+        engines.push_back(engine);
+    }
 }
 
 void StageOne::activate() {
@@ -28,6 +42,85 @@ void StageOne::deactivate() {
     for (int i = 0; i < engines.size(); ++i) {
         engines[i]->turnOff();
     }
+}
+
+void StageOne::testEngines()
+{
+    cout<<"Initiating static fire test..."<<endl;
+    sleep(1);
+    for(int i=0;i<numEngines;i++)
+    {
+        engines[i]->turnOn();
+    }
+    cout<<"All engines have been initiated..."<<endl;
+    cout<<endl;
+    
+    srand(time(0));
+    for(int i=0;i<numEngines;i++)
+    {
+        int rand;
+        rand= (std::rand()%10)+1;
+        if(rand%2==0)
+        {
+            EngineOptimisation* state = new EngineOptimum();
+            engines[i]->setReadyState(state);
+        }
+        else
+        {
+            EngineOptimisation* state = new EngineNotOptimum();
+            engines[i]->setReadyState(state);
+        }
+        
+    }
+    printEngines();
+    cout<<endl;
+
+    if(determineOptimisation()==true)
+    {
+        cout<<"All engines are optimum and have passed the test, proceed to launch..."<<endl;
+    }
+    else{
+        cout<<"Enigines have failed the test, tweaking engines..."<<endl;
+        for(int i=0;i<numEngines;i++)
+        {
+            EngineOptimisation* state = new EngineOptimum();
+            engines[i]->setReadyState(state);
+        }
+        sleep(2.5);
+        cout<<endl;
+        printEngines();
+        cout<<"All engines are optimum and are ready to launch..."<<endl;
+    }
+
+}
+
+void StageOne::printEngines()
+{
+    for(int i=0;i<numEngines;i++)
+    {
+        
+        cout<<engines[i]->getId()<<" is ";
+        engines[i]->changeReadyState();
+        cout<<endl;
+        sleep(1);
+    }
+}
+
+bool StageOne::determineOptimisation()
+{
+    for(int i=0;i<numEngines;i++)
+    {
+       if(engines[i]->getReadyState()=="not Optimum")
+       {
+           return false;
+       }
+    }
+    return true;
+}
+
+int StageOne::setNum(int s)
+{
+    numEngines=s;
 }
 
 StageOne::~StageOne() = default;
