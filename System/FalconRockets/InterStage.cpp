ec63cb74165fae980227015f9bd82abb11d5544a
u18335412@tuks.co.za <u18335412@tuks.co.za>
2021-11-18 14:36:17 +0200

    added output for separation

--- System/FalconRockets/InterStage.cpp
+++ System/FalconRockets/InterStage.cpp
@@ -11,11 +11,14 @@ void InterStage::setGridFins(int) {
 }
 
 void InterStage::separate() {
-    cout << "Stage One and Stage Two are now separating" << endl;
+    cout << endl;
+    cout << "Separation Stage has begun: " << endl;
+    cout << "Stage One and Stage Two are now separating ";
     for (int i = 5; i > 0; --i) {
         cout << "<-> ";
         this_thread::sleep_for(chrono::seconds(1));
     }
+    cout << "\nStage One and Stage Two are now detached.\nFirst Stage is now returning back to earth." << endl;
 }
 
 void InterStage::activate() {
