387f4a74066c14c06981dfadddd487a15d108a16
Farai <Farai.fantasy.822@gmail.com>
2021-11-18 23:39:24 +0200

    Update Mark2Creator.cpp

--- System/DragonSpacecraft/code/Mark2Creator.cpp
+++ System/DragonSpacecraft/code/Mark2Creator.cpp
@@ -3,12 +3,17 @@
 
 using namespace std;
 
-Mark2Creator::Mark2Creator() {
+//@brief Consructor Creates a Mark2 Parachute
+Mark2Creator::Mark2Creator() 
+{
 	cout<<"create Mark2 Parachutes."<<endl;
 }
 
-Parachute* Mark2Creator::createParachute(string a) {
+//@brief Creates a Mark2 Parachute with a specific ID
+//@param a is the ID of the Parachute
+//@return returns a new Mark2Parachute with the given ID
+Parachute* Mark2Creator::createParachute(string a) 
+{
 	cout<<"Mark2 Parachute has been created"<<endl;
 	return new Mark2Parachute(a);
-
 }
