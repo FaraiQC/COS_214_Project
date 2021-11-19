6c69e8b8485a30e7d21f5de2e59d174fc5c9cb61
Farai <Farai.fantasy.822@gmail.com>
2021-11-19 14:22:35 +0200

    Update CrewMember.cpp

--- System/DragonSpacecraft/CrewMember.cpp
+++ System/DragonSpacecraft/CrewMember.cpp
@@ -3,28 +3,39 @@
 
 using namespace std;
 
+//@brief Constructor
 CrewMember::CrewMember() {
 	
 }
 
+//@brief Sets the name and the weight of the crew member
+//@param name is the name of the crew member
+//@param weight is the weight of the crew member
 CrewMember::CrewMember(string name, double weight) {
 	setName(name);
 	setWeight(weight);
 }
 
+//@brief Sets the name of the crew member
+//@param name is the name of the crew member
 void CrewMember::setName(string name) {
 	this->name=name;	
 }
 
+//@brief Sets the weight of the crew member
+//@param weight is the weight of the crew member
 void CrewMember::setWeight(double weight) {
 	this->weight=weight;
 }
 
+//@brief gets the name of the crew member
+//@return returns the name of the crew member
 string CrewMember::getName() {
 	return name;
 }
 
+//@brief gets the weight of the crew member
+//@return returns the weight of the crew member
 double CrewMember::getWeight() {
 	return this->weight;
 }
-
