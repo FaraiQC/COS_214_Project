bbede3c22451451337d25471a87426f40c7c965f
Farai <Farai.fantasy.822@gmail.com>
2021-11-19 14:28:50 +0200

    Update DracoCreator.cpp

--- System/DragonSpacecraft/DracoCreator.cpp
+++ System/DragonSpacecraft/DracoCreator.cpp
@@ -1,5 +1,8 @@
 #include "DracoCreator.h"
 
+//@brief Creates a new Draco with an ID
+//@param i is the ID of the Draco to be created
+//@return returns the new Draco that was created
 Thruster* DracoCreator::createThruster(string i){
 	return new Draco(i);
 }
