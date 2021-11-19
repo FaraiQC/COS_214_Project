3f937ba5ae725f41d3e8d20c19092996415998a1
Farai <Farai.fantasy.822@gmail.com>
2021-11-19 14:57:30 +0200

    Update Mark3Creator.cpp

--- System/DragonSpacecraft/Mark3Creator.cpp
+++ System/DragonSpacecraft/Mark3Creator.cpp
@@ -3,9 +3,14 @@
 
 using namespace std;
 
+//@brief Constructor
 Mark3Creator::Mark3Creator() {
 	
 }
+
+//@brief creates a Mark3Parachute with a specific id
+//@param a is the id of the Parachute
+//@return returns the newly created Mark3Parachute
 Parachute* Mark3Creator::createParachute(string a) {
 	cout<<"Mark3 Parachute has been created"<<endl;
 	return new Mark3Parachute(a);
