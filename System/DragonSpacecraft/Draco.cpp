0cabc69f0e81251589b7145720f9fc838d115e9b
Farai <Farai.fantasy.822@gmail.com>
2021-11-19 14:24:37 +0200

    Update Draco.cpp

--- System/DragonSpacecraft/Draco.cpp
+++ System/DragonSpacecraft/Draco.cpp
@@ -3,12 +3,15 @@
 
 using namespace std;
 
+//@brief Constructor initialises a Draco switched off with a power of 80, but is not cereated 
 Draco::Draco() {
 	turnOff();
 	this->setPower(80);
 	SetDracoisCreatedChecker(0);
 }
 
+//@brief Constructor initialises a Draco switched off with a power of 80 and an id, but is not created
+//@param id is the ID of the Draco
 Draco::Draco(string id) {
 	SetDracoisCreatedChecker(0);
 	this->setPower(80);
@@ -16,6 +19,7 @@ Draco::Draco(string id) {
 	turnOff();
 }
 
+//@brief Switches on the Draco
 void Draco::fireUp() {
 	if(isOn() == true){
 		cout<<getId()<<" turned on, firing up!"<<endl;
@@ -25,6 +29,7 @@ void Draco::fireUp() {
 	}	
 }
 
+//@brief Shuts down the Draco
 void Draco::shutDown() {
 	this->turnOff();
 	cout<<getId()<<" has been shut down"<<endl;
