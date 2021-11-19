4d3002ee843285b6740efd0a001c351d9308ee3d
Farai <Farai.fantasy.822@gmail.com>
2021-11-19 14:58:54 +0200

    Update Mark3Parachute.cpp

--- System/DragonSpacecraft/Mark3Parachute.cpp
+++ System/DragonSpacecraft/Mark3Parachute.cpp
@@ -3,15 +3,19 @@
 
 using namespace std;
 
+//@brief Constructor
 Mark3Parachute::Mark3Parachute() {
 	
 }
 
+//@brief Constructor initializes the id of the Parachute
+//@param name is the id of the parachute
 Mark3Parachute::Mark3Parachute(string name) {
 	setId(name);
 	SetMark2isCreatedChecker(0);
 }
 
+//@brief Displays that Mark3Parachute is being deployed
 void Mark3Parachute::deploy() {
 	cout<<"Now deploying Mark3Parachute."<<endl;
 }
