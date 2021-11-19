bf0cd39c0188af58177ea29ec5b014791d04f609
Farai <Farai.fantasy.822@gmail.com>
2021-11-19 15:15:32 +0200

    Update SuperDracoCreator.cpp

--- System/DragonSpacecraft/SuperDracoCreator.cpp
+++ System/DragonSpacecraft/SuperDracoCreator.cpp
@@ -1,5 +1,7 @@
 #include "SuperDracoCreator.h"
 
+//@brief Creates a SuperdDraco with a specific ID
+//@return returns the newly created Super Draco
 Thruster* SuperDracoCreator::createThruster(string id){
 	return new SuperDraco(id);
 }
