6fcece5b1abcf2a0ce04c233d9cb00ab19d93703
Farai <Farai.fantasy.822@gmail.com>
2021-11-19 14:56:22 +0200

    Update Mark2Parachute.cpp

--- System/DragonSpacecraft/Mark2Parachute.cpp
+++ System/DragonSpacecraft/Mark2Parachute.cpp
@@ -5,14 +5,19 @@
 
 using namespace std;
 
+//@brief Constructor
 Mark2Parachute::Mark2Parachute() {
 
 }
 
+//@brief Sets the ID of the Parachute and checks that it is created, which it is.
+//@param name is the ID of the Parachute
 Mark2Parachute::Mark2Parachute(string name) {
 	setId(name);
 	SetMark2isCreatedChecker(1);
 }
+
+//@brief Displays that Mark2Parachute is now being deployed
 void Mark2Parachute::deploy() {
 	cout<<"Now deploying Mark2Parachute."<<endl;
 }
