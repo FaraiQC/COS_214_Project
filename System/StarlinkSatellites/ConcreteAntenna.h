f11f240b780e539bcd4f2ad1a846cfbbdf3b96cc
Kuda Christine <62636817+Kuda214@users.noreply.github.com>
2021-11-21 18:34:45 +0200

    Update ConcreteAntenna.h

--- System/StarlinkSatellites/ConcreteAntenna.h
+++ System/StarlinkSatellites/ConcreteAntenna.h
@@ -1,15 +1,17 @@
 #ifndef CONCRETEANTENNA_H
 #define CONCRETEANTENNA_H
 #include "Antenna.h"
-#include "ConcreteAntenna.h"
+#include "concreteSatellite.h"
 #include <iostream>
+
 using namespace std;
 
 class ConcreteAntenna : public Antenna {
 
 private:
 	bool radioSignal;
-	ConcreteSatellite* subject;
+	concreteSatellite* subject = new concreteSatellite();
+
 
 public:
 	void update();
