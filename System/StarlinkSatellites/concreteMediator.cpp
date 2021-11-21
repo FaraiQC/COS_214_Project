2248b21e3e78d2bcbc7696626c4aa79c36e8d23b
Kuda Christine <62636817+Kuda214@users.noreply.github.com>
2021-11-21 18:38:00 +0200

    Update concreteMediator.cpp

--- System/StarlinkSatellites/concreteMediator.cpp
+++ System/StarlinkSatellites/concreteMediator.cpp
@@ -6,15 +6,7 @@ using namespace std;
 
 //@brief Spreads the satellites evenly then switches off all of their lasers
 void concreteMediator::notify() 
-{
-    /*
-    Satellite* curr = satelliteList->nextSatellite();
-    while(curr != NULL)
-    {
-        curr->setDistance();
-        curr = satelliteList->nextSatellite();
-    }*/
-    
+{   
     cout<<"Configuration done! All Satellites are spread equally.\n";
     cout<<"Satellite's lasers have been turned Off.\n";
 
