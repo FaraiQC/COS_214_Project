648055e1e3d1b1a15ab44d9d5953ac7efe352797
Kuda Christine <62636817+Kuda214@users.noreply.github.com>
2021-11-21 19:20:37 +0200

    Update satellitesMaker.h

--- System/StarlinkSatellites/satellitesMaker.h
+++ System/StarlinkSatellites/satellitesMaker.h
@@ -1,19 +1,75 @@
-#ifndef SATELLITESMAKER_H
-#define SATELLITESMAKER_H
-#include "Satellite.h"
+#include "satellitesMaker.h"
+#include <iostream>
 
-class satellitesMaker {
+using namespace std;
 
-private:
-	Satellite* first;
-	bool empty;
+satellitesMaker::satellitesMaker() 
+{
+	// this->first = new Satellite();
+	first = NULL;
+	
+}
 
-public:
-	satellitesMaker();
+void satellitesMaker::CollectNextCluster() 
+{
+	//var
+	Satellite* p = new Satellite() ;
+	Satellite* curr = first;
+	Satellite* prev = NULL;
+	
+	//make 1 Satellite
+	this->first = p;
+	setFist(p);
+	first-> prevS = NULL;
 
-	void CollectNextCluster();
+	//prototype 59 Satellites using prototype and iterator 
+	curr = first;
+	for (int n=0;n<60;n++)
+	{
+		curr->nextS = p->Clone();
+ 
+		prev = curr;
+		curr = curr->nextS;
 
-	bool isEmpty();
-};
+		curr->prevS= prev;
 
-#endif
+	}
+	
+	this->empty = true;
+}
+
+bool satellitesMaker::isEmpty() 
+{
+	if(first == NULL)
+	{
+		return true;
+	}
+	return false;
+}
+
+Satellite* satellitesMaker :: testCluster()
+{
+	CollectNextCluster();
+	return first;
+}
+
+void satellitesMaker :: satellitesBeforeLaunch()
+{
+	CollectNextCluster();
+	if(isEmpty() == false)
+	{
+		cout<<"Satellite cluster is ready to be launched.\n";
+	}
+}
+
+Satellite* satellitesMaker :: getFirst()
+{
+	// CollectNextCluster();
+	return first;
+}
+
+void satellitesMaker :: setFist(Satellite* s)
+{
+	this->first = s;
+}
+
