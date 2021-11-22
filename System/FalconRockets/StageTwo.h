4710f4c53d7c8ac562c5c5feb03d96f99022a298
Kuda Christine <62636817+Kuda214@users.noreply.github.com>
2021-11-22 15:35:49 +0200

    Update StageTwo.h

--- System/FalconRockets/StageTwo.h
+++ System/FalconRockets/StageTwo.h
@@ -3,28 +3,36 @@
 
 #include "Stage.h"
 #include "../DragonSpacecraft/Spacecraft.h"
-#include "../StarlinkSatellites/Satellite.h"
+//#include "../StarlinkSatellites/Satellite.h"
 #include "VacuumEngine.h"
 #include "EngineNotOptimum.h"
 #include "EngineOptimum.h"
-
-//#include "Spacecraft.h"
-
+#include "../StarlinkSatellites/satellitesMaker.h"
+#include "../LaunchSimulator/TestSatellites.h"
 
 class StageTwo : public Stage {
 private:
     int numEngines;
     Spacecraft *capsule;
-    Satellite* satellite;
+    TestSatellites* satellites;
     vector<Engine *> engines;
+    int fuel;
 public:
     explicit StageTwo(string id);
 
     void addEngine(Engine* engine);
 
-    void attachSpacecraft(Spacecraft* spaceCraft);
+    bool attachSpacecraft(Spacecraft* spaceCraft);
 
-    void attachSatellite(Satellite* satellite);
+    bool attachSatellite(TestSatellites* satellites);
+
+    bool HasSpacecraft();
+
+    bool HasSatellites();
+
+    TestSatellites* getSatellites();
+
+    Spacecraft* getSpacecraft();
 
     void detachSpacecraft();
 
@@ -41,6 +49,10 @@ public:
     void printEngines();
 
     bool determineOptimisation();
+
+    void setFuel(int f);
+
+    int getFuel();
 
     ~StageTwo() override;
 };
