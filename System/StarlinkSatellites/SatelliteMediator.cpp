ceccd035f71854c3b9cb10709bdaee6b30d24e88
N4T1V3 <elphusurous47@gmail.com>
2021-11-23 03:48:20 +0000

    Congruent Commits

--- System/StarlinkSatellites/SatelliteMediator.cpp
+++ System/StarlinkSatellites/SatelliteMediator.cpp
@@ -1,10 +1,19 @@
 #include "SatelliteMediator.h"
 
+
+/**
+ * @brief Construct a new Satellite Mediator:: Satellite Mediator object
+ * 
+ */
 SatelliteMediator::SatelliteMediator() 
 {
 
 }
 
+/**
+ * @brief Destroy the Satellite Mediator:: Satellite Mediator object
+ * 
+ */
 SatelliteMediator::~SatelliteMediator() 
 {
 
